102b581e004cfd8f3a106e325a5dca050d48840f
Evandro P. Santos <evandro.santos@soulisto.com.br>
2020-05-25 08:53:32 -0300

    Component message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ErrorsModule } from './errors/errors.module';
     AppRoutingModule,
     PhotosModule,
     ErrorsModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
