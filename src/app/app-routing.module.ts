18d54f8b3eb4507ba0ab1d4b699067438964053e
Evandro P. Santos <evandro.santos@soulisto.com.br>
2020-05-28 18:01:27 -0300

    Child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,25 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
+import { SignUpComponent } from './home/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: SignInComponent
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: SignInComponent
+      },
+      {
+        path: 'signup',
+        component: SignUpComponent
+      },
+    ]
   },
   {
     path: 'user/:userName',
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     VMessageModule,
     RouterModule
   ]
